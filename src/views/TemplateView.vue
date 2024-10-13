9302c91c2ea9e6d0f4c960eb44aacf8cc453c05d
RerykA <0ta5274738t4p4r@gmail.com>
2024-10-13 21:56:49 +0900

    テンプレートを2パターン用意

--- src/views/TemplateView.vue
+++ src/views/TemplateView.vue
@@ -1,65 +1,9 @@
 <script setup>
-import {
-  ref,
-  reactive,
-  watch,
-  computed,
-  onActivated,
-  onMounted,
-  onUnmounted,
-} from 'vue'
-
-// リアクティブな変数には
-// ref・reactiveの２パターン書き方がある
-const message = ref('Hello world')
-const user = reactive({
-  name: 'User name',
-  age: 999,
-})
-
-watch(
-  () => message.value,
-  () => {
-    // ここに処理を書く
-  },
-)
-
-const actionName = computed(() => {
-  // ここに処理を書く
-  return 'message'
-})
-
-function onClickAction() {
-  // ここに処理を書く
-}
-
-const myAction = () => {
-  // 関数はこのように書くこともできる
-}
-
-onActivated(() => {
-  // ここに初期処理を書く
-})
-
-onMounted(() => {
-  // ここに初期処理を書く
-})
-
-onUnmounted(() => {
-  // ここに画面を去る際の処理を書く
-})
+// ここにScriptを追加する
 </script>
 
 <template>
-  <span>{{ message }}</span>
-
-  <div>
-    {{ user }}
-    <div>name: {{ user.name }}</div>
-    <div>age: {{ user.age }}</div>
-  </div>
-
-  <button @click="onClickAction">Button</button>
+  <!-- ここにHTMLを追加する -->
 </template>
 
 <style>
